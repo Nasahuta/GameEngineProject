acebfb6ca8813b056d86d3ceee17220b5c1cad4f
Nasahuta <joni.alanko@cs.tamk.fi>
2018-11-07 15:46:18 +0200

    Added author print.

--- Game/src/main/java/fi/tamk/tiko/olioohjelmointi/Sovellus.java
+++ Game/src/main/java/fi/tamk/tiko/olioohjelmointi/Sovellus.java
@@ -3,6 +3,6 @@ package fi.tamk.tiko.olioohjelmointi;
 public class Sovellus {
 
     public static void main(String [] args) {
-        
+        System.out.println("Author: Joni Alanko");
     }
 }
