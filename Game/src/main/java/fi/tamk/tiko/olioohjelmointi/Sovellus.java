76b9df97bc8b992c8621088e06be443e8605cfea
Joni Alanko <joni.alanko@cs.tamk.fi>
2018-11-14 00:21:14 +0200

    JavaFX testing

--- Game/src/main/java/fi/tamk/tiko/olioohjelmointi/Sovellus.java
+++ Game/src/main/java/fi/tamk/tiko/olioohjelmointi/Sovellus.java
@@ -1,8 +1,32 @@
 package fi.tamk.tiko.olioohjelmointi;
 
-public class Sovellus {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+public class Sovellus extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setHeight(480);
+        stage.setWidth(640);
+        stage.show();
+    }
 
     public static void main(String [] args) {
         System.out.println("Author: Joni Alanko");
+        Player testi = new Player();
+
+        launch(args);
+    }
+}
+
+class Player {
+    private int xPosition;
+    private int yPosition;
+    private Image model;
+
+    public Player() {
+        model = new Image("fi/tamk/tiko/assets/test.jpg");
     }
 }
